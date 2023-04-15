fa90bff9c889660edc236410de3297a7d9f759ff
littlestorm120 <ruan_xuewu@163.com>
2023-04-15 23:15:49 +0800

    Update default.ts

--- 来源/default.ts
+++ 来源/default.ts
@@ -7,6 +7,5 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用555。
-- 由 [是枭](https://blog.evv1.com) 站点源码出售，功能强大响应快，欢迎自部署。
+- 本站仅用于演示demo，填入正确的  openai key 才可使用，如果没有，可以联系qq332595319获取。
 - **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
